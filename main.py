071b84384f5deaa502c4f99ad5e9414731c8af53
SOFYA <world.is.unpredictable@gmail.com>
2023-06-11 17:24:31 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+def strcounter(s):  # решение за N
+    syms_counter = {}
+    for sym in s:
+        syms_counter[sym] = syms_counter.get(sym, 0) + 1
+
+    for sym, count in syms_counter.items():
+        print(sym, count)
+
+
+strcounter('aabbbbccd')
